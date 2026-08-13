f5c173d577ee4d4d2da9e31c5ecd8785f39070fc
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-13 16:24:05 +0900

    Update path for GROMACS trjconv command

--- 10_PROC-GROMACS-CONV4.sh
+++ 10_PROC-GROMACS-CONV4.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd 03_GROMACS/05_PROC-GROMACS
 
-gmx trjconv -s md.tpr -f md_center.xtc -n index.ndx -o md_fit.xtc -center -fit rot+trans
+gmx trjconv -s ../04_MD/md.tpr -f md_center.xtc -n index.ndx -o md_fit.xtc -center -fit rot+trans
 
 # 11
 # 11
