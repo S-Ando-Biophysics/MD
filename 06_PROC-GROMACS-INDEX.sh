0508176319f12f02cbdaa40af09dcaefd9dc300e
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-13 16:21:36 +0900

    Update 06_PROC-GROMACS-INDEX.sh

--- 06_PROC-GROMACS-INDEX.sh
+++ 06_PROC-GROMACS-INDEX.sh
@@ -5,12 +5,7 @@ mkdir -p 03_GROMACS/05_PROC-GROMACS
 
 cd 03_GROMACS/05_PROC-GROMACS
 
-cp ../04_MD/md.gro .
-cp ../04_MD/md.tpr .
-cp ../04_MD/md.xtc .
-cp ../04_MD/md.edr .
-
-gmx make_ndx -f md.tpr -o index.ndx
+gmx make_ndx -f ../04_MD/md.tpr -o index.ndx
 
 # l
 # ri 1-○○
