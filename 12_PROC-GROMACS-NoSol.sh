990a0402bfd06b977b90b8f1da4de3823c537b7e
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-13 16:24:58 +0900

    Update 12_PROC-GROMACS-NoSol.sh

--- 12_PROC-GROMACS-NoSol.sh
+++ 12_PROC-GROMACS-NoSol.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd 03_GROMACS/05_PROC-GROMACS
 
-gmx trjconv -s md.tpr -f md_fit.xtc -n index.ndx -o md_fit_nosol.xtc
+gmx trjconv -s ../04_MD/md.tpr -f md_fit.xtc -n index.ndx -o md_fit_nosol.xtc
 
 awk '
 function trim(s) {
