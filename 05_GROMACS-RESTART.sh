5a1b67a1fda840cb865f5e33bb5a6a618c1ac914
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-21 17:29:11 +0900

    Enhance restart script with status messages

--- 05_GROMACS-RESTART.sh
+++ 05_GROMACS-RESTART.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo
+echo "========================================"
+echo " Restart production MD "
+echo "========================================"
+echo
+
 cd 03_GROMACS/04_MD
 
 gmx mdrun -v -deffnm md -nb auto -nbfe auto -pme auto -pmefft auto -bonded auto -update auto -cpi md.cpt
 
 cd ../..
 
-echo "=== Finished ==="
+echo
+echo "========================================"
+echo " Finished "
+echo "========================================"
+echo
