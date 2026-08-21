aff2222bb95bc923fde4196e0811c86f89a7a544
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-21 14:03:11 +0900

    Update APPX_PROC-AMBER.sh

--- APPX_PROC-AMBER.sh
+++ APPX_PROC-AMBER.sh
@@ -22,9 +22,9 @@ rms first :${FIT_RES}&!@H= out rmsd_fit_nosol.dat
 
 strip :WAT,SOL,Na+,NA+,NA,K+,K,Cl-,CL-,CL
 
-trajout md_fit_nosol.nc netcdf
-trajout md_fit_nosol.xtc xtc
-trajout md_nosol.pdb pdb onlyframes 1
+trajout md_fit_target.nc netcdf
+trajout md_fit_target.xtc xtc
+trajout md_target.pdb pdb onlyframes 1
 
 run
 quit
