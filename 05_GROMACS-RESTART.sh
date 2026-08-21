cf33263090ec7be15854c37d7f966fb4c754405c
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-08-21 12:51:21 +0900

    Update 05_GROMACS-RESTART.sh

--- 05_GROMACS-RESTART.sh
+++ 05_GROMACS-RESTART.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd 03_GROMACS/04_MD
 
-gmx mdrun -v -deffnm md -nb auto -cpi md.cpt
+gmx mdrun -v -deffnm md -nb auto -nbfe auto -pme auto -pmefft auto -bonded auto -update auto -cpi md.cpt
 
 cd ../..
 
